474b89447183a293880bd29b7a28e31eae597523
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-08-09 21:53:13 +0500

    Initial commit

---
+++ src/test/java/ru/maxima/finalproject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maxima.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
