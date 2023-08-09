2a1e7f785328af8f5ca35882b840187660b36acd
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-08-09 21:54:48 +0500

    person model

--- src/main/java/ru/maxima/finalproject/model/Person.java
+++ src/main/java/ru/maxima/finalproject/model/Person.java
@@ -1,2 +1,19 @@
-package ru.maxima.finalproject.model;public class Person {
+package ru.maxima.finalproject.model;
+
+import java.time.LocalDateTime;
+
+public class Person {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime removedAt;
+    private String createdPerson;
+    private String removedPerson;
+
+
 }
