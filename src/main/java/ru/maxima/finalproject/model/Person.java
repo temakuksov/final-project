7db44014b84da8040261eaa9ef7c3b50bad297da
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-08-10 21:39:08 +0500

    person model

--- src/main/java/ru/maxima/finalproject/model/Person.java
+++ src/main/java/ru/maxima/finalproject/model/Person.java
@@ -7,15 +7,14 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @Data
 @NoArgsConstructor
 @Entity
-
 public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
     private String name;
     private Integer age;
