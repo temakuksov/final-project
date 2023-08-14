3f7405cfe327faef9e12ffc6bed0cf471ae7154e
Kuksov Artem <35718107+temakuksov@users.noreply.github.com>
2023-08-15 01:25:55 +0500

    Spring Security + JWT (works add user) + all ative books

--- src/main/java/ru/maxima/finalproject/repository/PersonRepo.java
+++ src/main/java/ru/maxima/finalproject/repository/PersonRepo.java
@@ -6,14 +6,13 @@ import ru.maxima.finalproject.model.Person;
 
 import java.util.Optional;
 
-public interface PersonRepo extends JpaRepository <Person,Long> {
+public interface PersonRepo extends JpaRepository<Person, Long> {
 
     @Query("select p.name from #{#entityName} p where p.id=?1")
     String findBy(Long adminId);
 
     @Query
     Optional<Person> findByEmail(String email);
-
 
 
 }
